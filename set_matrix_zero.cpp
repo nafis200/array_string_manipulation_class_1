e36699a56c871ab35ef30bcba7ba6f083056fb43
nafis ahamed <nafisahamed14@gmail.com>
2026-03-08 17:03:28 +0600

    matrix

--- set_matrix_zero.cpp
+++ set_matrix_zero.cpp
@@ -29,9 +29,26 @@ using namespace std;
 //   }
 // }
 
+// space 0(1) time o(n^2)
+
 void rotate(vector<vector<int>>& matrix) {
   int n = matrix.size();
   int m = matrix[0].size();
+
+  bool row = false, col = false;
+
+  for (int i = 0; i < n; i++) {
+    if (matrix[i][0] == 0) {
+      row = true;
+    }
+  }
+
+  for (int j = 0; j < m; j++) {
+    if (matrix[0][j] == 0) {
+      col = true;
+    }
+  }
+
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       if (matrix[i][j] == 0) {
@@ -43,11 +60,21 @@ void rotate(vector<vector<int>>& matrix) {
   for (int i = 1; i < n; i++) {
     for (int j = 1; j < m; j++) {
       if (matrix[i][0] == 0) {
-          matrix[i][j] = 0;
+        matrix[i][j] = 0;
       }
       if (matrix[0][j] == 0) {
         matrix[i][j] = 0;
       }
+    }
+  }
+  if (row) {
+    for (int i = 0; i < n; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+  if (col) {
+    for (int i = 0; i < m; i++) {
+      matrix[0][i] = 0;
     }
   }
 }
